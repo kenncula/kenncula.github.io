02f63d13cfdb805b566fb9219a429de7f15272da
Kenneth Cula <kenncul123@gmail.com>
2025-01-13 00:55:27 -0500

    mobile friendly about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -24,12 +24,12 @@ const About = () => {
   return (
     <div id="About" className="flex flex-col items-center justify-center min-h-screen" style={{ backgroundColor: '#434343' }}>
       <div className="flex flex-row items-center justify-center min-h-screen">
-        <div className="flex flex-col items-center justify-center flex-grow mr-32 ml-32">
-          <h1 className="text-6xl font-bold text-white">
+        <div className="flex flex-col items-center justify-center flex-grow mx-4 md:mr-32 md:ml-32">
+          <h1 className="text-4xl md:text-6xl  font-bold text-white text-center md:text-left">
             Hello, I&apos;m (just) Ken
           </h1>
-          <div className="text-xl text-gray-300 text-justify">
-            <p className="mt-10">
+          <div className="text-lg md:text-xl text-gray-300 text-justify mt-4 md:mt-10">
+            <p className="mt-4 md:mt-0">
               I am passionate about secure, distributed systems that operate 
               harmoniously as a symphony.  </p>
             <p className="mt-3">
@@ -43,7 +43,7 @@ const About = () => {
         <img 
           src={imgSrcs[imgId]} 
           alt="personal picture" 
-          className={`w-1/3 ml-auto mr-10 transition duration-500 ease-in-out transform ${isTransformed ? 'scale-x-[-1] ' : 'scale-x-[1]'}`}
+          className={`w-2/3 md:w-1/3 ml-8 md:mt-0 mr-10 transition duration-500 ease-in-out transform ${isTransformed ? 'scale-x-[-1] ' : 'scale-x-[1]'}`}
           onClick={handlePicClick}/>
       </div>
     </div>
